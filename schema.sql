e25dec5efe664c7a12a343d1bc3a733e0bcea2e9
Liam Houston <liamhouston@Liams-MacBook-Air.local>
2021-08-10 16:16:16 -0400

    Made first draft schema

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE productsDB;
+
+USE productsDB;
+
+CREATE TABLE products (
+  _id int PRIMARY KEY
+  _ProductName text,
+  _Slogan text,
+  _ProductDescription text,
+  _Category text,
+  _Price int
+);
+
+CREATE TABLE features (
+  _FeatureID int PRIMARY KEY,
+  _Features text,
+  _Value text
+)
+
+CREATE TABLE productFeatureRealtion (
+  _ProductID int,
+  _FeatureID int
+)
+
+CREATE TABLE relatedProducts (
+  _id int PRIMARY KEY,
+  _OriginProduct int
+);
