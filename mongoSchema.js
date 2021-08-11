ddffd73fe37157cbac1b011a555a842f44eefaf9
Liam Houston <liamhouston@Liams-MacBook-Air.local>
2021-08-11 01:15:10 -0400

    initial schemas

---
+++ mongoSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Products = new mongoose.Schema({
+  _ProductName {
+    type: string,
+  },,
+  _Slogan {
+    type: string,
+  },,
+  _ProductDescription {
+    type: string,
+  },,
+  _Category {
+    type: string,
+  },,
+  _Price {
+    type: Number,
+  },
+})
+
+const Features = new mongoose.Schema({
+  Features {
+    type: string,
+  },,
+  Values {
+    type: string,
+  }
+})
+
+
+const RelatedProducts = new mongoose.Schema({
+  OriginProducts {
+    type: Number,
+  }
+})
